c8044ecfadf3dda6bbb45f7b92a8892b56e3fcda
Stefano Montrasio <montrasio97@gmail.com>
2023-05-21 10:15:01 +0200

    T-0: initialized spring boot app

---
+++ src/main/java/codeswitch/italiocco/ItalioccoApplication.java
@@ -0,0 +1,13 @@
+package codeswitch.italiocco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItalioccoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItalioccoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeswitch/italiocco/ItalioccoApplicationTests.java
@@ -0,0 +1,13 @@
+package codeswitch.italiocco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItalioccoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
